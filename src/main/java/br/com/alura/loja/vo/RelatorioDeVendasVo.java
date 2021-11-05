04dd1b1fe7d16238f37a616be31bfe6703c77078
Nailton2002 <jnmp25jnmp@gmail.com>
2021-11-05 15:36:15 -0300

    jpa

---
+++ src/main/java/br/com/alura/loja/vo/RelatorioDeVendasVo.java
@@ -0,0 +1,36 @@
+package br.com.alura.loja.vo;
+
+import java.time.LocalDate;
+
+//ESTA CLASE NÃO É UMA ENTIDADE, UM OBJETO DE VALOR, ELA REPRESENTA UM RELATORIO
+public class RelatorioDeVendasVo {
+	
+	private String nomeProduto;
+	private Long quantidadeVendida;
+	private LocalDate dataUltimaVenda;
+	
+	public RelatorioDeVendasVo(String nomeProduto, Long quantidadeVendida, LocalDate dataUltimaVenda) {
+		this.nomeProduto = nomeProduto;
+		this.quantidadeVendida = quantidadeVendida;
+		this.dataUltimaVenda = dataUltimaVenda;
+	}
+	
+	@Override
+	public String toString() {
+		return "RelatorioDeVendasVo [nomeProduto=" + nomeProduto + ", quantidadeVendida=" + quantidadeVendida
+				+ ", dataUltimaVenda=" + dataUltimaVenda + "]";
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public Long getQuantidadeVendida() {
+		return quantidadeVendida;
+	}
+
+	public LocalDate getDataUltimaVenda() {
+		return dataUltimaVenda;
+	}
+	
+}
